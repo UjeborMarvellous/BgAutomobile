2a1ee96bfa0d025e5b98e29f9a91b9223cf139f0
UjeborMarvellous <92124127+UjeborMarvellous@users.noreply.github.com>
2023-05-11 11:56:08 +0100

    first commit

---
+++ src/Components/LandingPage.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+
+function LandingPage() {
+  const imageDirectory = "./Images/";
+  const images = ["Lambo.png", "Blue.jpg", "Lamboside.png", "LamboBack.png"];
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [images.length]);
+  return (
+    <div className="landing">
+      <div className="grid grid-cols-1 md:grid md:grid-cols-2 px-16">
+        <div className="Land pt-36">
+          <h1 className="text-white text-6xl h1font">
+            Car Dealing Experience. Redefined!
+          </h1>
+          <p className="text-white mt-8 text-sm">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi id
+            esse fugiat deleniti. Beatae in nostrum ut at qui, labore, vel rerum
+            quas velit dicta accusamus, dolorum cupiditate distinctio nobis!
+          </p>
+          <div className="btns my-8 lg:flex">
+            <button className="bg-blue-500 py-3 sm:-pt-10 mr-4 lg:py-3 rounded-sm px-10 text-white font-san text-sm w-4/5 uppercase hover:bg-transparent hover:border-2 hover:border-blue-500">
+              View Products
+            </button>
+            <button className="border-2 border-white text-white py-3 px-10 uppercase text-sm w-4/5 md:ml-4 ml-0 md:mt-0 mt-6 hover:bg-blue-500 hover:border-none ">
+              Contact Us
+            </button>
+          </div>
+        </div>
+        <div className="Land">
+          <img
+            src={imageDirectory + images[currentImageIndex]}
+            alt="Logo"
+            className="w-full CarImage"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  const Links = [
+    { name: "Home", link: "/" },
+    { name: "Shop", link: "/Shop" },
+    { name: "About", link: "/" },
+    { name: "Accessories", link: "/" },
+    { name: "Offers", link: "/" },
+    { name: "Brands", link: "/" },
+  ];
+  const [open,setOpen] = useState(false);
+  return (
+    <div>
+      <nav className="nav shadow-md w-full fixed top-0 left-0">
+        <div className="md:flex items-center justify-between text-white capitalise py-0 lg:px-20 md:px-10 px-7">
+          <a href="/" className="nav">
+            <img src={"./Images/logo.png"} alt="Logo" className="w-32" />
+          </a>
+          <div className="Links">
+            <div className="flex">
+                <i onClick={() => setOpen(!open)} name={open ? 'x-circle' : 'bx-menu'} class='bx bx-menu-alt-right text-3xl absolute right-8 top-6 cursor-pointer md:hidden'></i>
+                {/* <i class='sec bx bx-search text-3xl ml-36 sm:mr-20 sm:absolute -right-12 sm:right-8 top-12 sm:top-8 cursor-pointer'></i> */}
+            </div>
+            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-490px]'} `}>
+              {Links.map((link) => (
+                <li key={link.name} className="md:ml-8 md text-xl md:my-0 my-4 lg:my-10">
+                  <a
+                    href={link.link}
+                    className="text-white lg:hover:duration-500 lg:hover:border-b-2 hover:ease-in lg:hover:border-blue-400 "
+                  >
+                    {link.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
