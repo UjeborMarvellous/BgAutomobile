b398233df1c3008d24d108d8ce6e0629271c6d3f
UjeborMarvellous <92124127+UjeborMarvellous@users.noreply.github.com>
2023-05-16 01:47:11 +0100

    Footer

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
   return (
     <div>
       <nav className="nav shadow-md w-full fixed top-0 left-0">
-        <div className="md:flex items-center justify-between text-white capitalise py-0 lg:px-20 md:px-10 px-7">
+        <div className="md:flex  items-center justify-between text-white capitalise py-0 lg:px-20 md:px-10 px-7">
           <a href="/" className="nav">
             <img src={"./Images/logo.png"} alt="Logo" className="w-32" />
           </a>
@@ -21,7 +21,7 @@ function Navbar() {
             <div className="flex">
                 <i onClick={() => setOpen(!open)} name={open ? 'x-circle' : 'bx-menu'} class='bx bx-menu-alt-right text-3xl absolute right-8 top-6 cursor-pointer md:hidden'></i>
             </div>
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-490px]'} `}>
+            <ul className={`md:flex md-flex md:items-center md:pb-0 pb-12 absolute md:static bg md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20':'top-[-490px]'} `}>
               {Links.map((link) => (
                 <li key={link.name} className="md:ml-8 md text-xl md:my-0 my-4 lg:my-0">
                   <a
